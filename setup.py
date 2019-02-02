f6a3d8ea35e8334ccf098b3cea53f4ee817d08ba
Daniel Lupu <lupu.daniel.f@gmail.com>
2019-02-03 01:45:56 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+from os import path
+
 from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='aiodecorators',
-    version='0.1',
-    description='Decorators for asyncio synchronization primitives',
+    version='0.2',
+    description='Function decorators based on asyncio synchronization primitives',
+    keywords='asyncio aio lock semaphore boundedsemaphore decorator decorators synchronization',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='http://github.com/dflupu/aiodecorators',
     author='Daniel Lupu',
     author_email='dflupu@gmail.com',
